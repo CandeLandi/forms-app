c94310d3cc25f5a3ae47c3874b7e4939dc1c112f
Candela Landi <landicandela01@gmail.com>
2024-04-11 16:36:59 -0600

    add validators service

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { cantBeStrider } from '../../../shared/validators/validators';
+import {EmailValidator, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as customValidators from '../../../shared/validators/validators';
+import { ValidatorsService } from '../../../shared/services/validators-service.service';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -10,21 +11,26 @@ export class RegisterPageComponent {
 
 
   public myForm: FormGroup = this.fb.group({
-    name: ['', [Validators.required]],
-    email: ['', [Validators.required]],
-    username: ['', [Validators.required, cantBeStrider ]],
+    name: ['', [Validators.required, Validators.pattern(this.validatorsService.firstNameAndLastnamePattern)]],
+/*     email: ['', [Validators.required, Validators.pattern(this.validatorsService.emailPattern)], [new EmailValidator() ]],
+ */    email: ['', [Validators.required, Validators.pattern(this.validatorsService.emailPattern)], [ this.emailValidator ]],
+    username: ['', [Validators.required, this.validatorsService.cantBeStrider]],
     password: ['', [Validators.required, Validators.minLength(6)]],
     password2: ['', [Validators.required]],
   })
 
 
-  constructor(private fb: FormBuilder) { }
+  constructor(
+    private fb: FormBuilder,
+    private validatorsService: ValidatorsService,
+    private emailValidator: EmailValidator
+  ) { }
 
-isValidField( field: string ) {
-  //TODO: obtener validación
-}
+  isValidField(field: string) {
+    return this.validatorsService.isValidField(this.myForm, field)
+  }
 
-  onSubmit(){
+  onSubmit() {
     this.myForm.markAllAsTouched();
   }
 
